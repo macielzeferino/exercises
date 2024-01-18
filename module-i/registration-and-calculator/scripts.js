af1b2325ac6ba892b50253ba8e796b52d62c0b6f
Maciel Zeferino <maciel.zeferino0@gmail.com>
2024-01-18 09:30:20 -0300

    module-i

---
+++ module-i/registration-and-calculator/scripts.js
@@ -0,0 +1,12 @@
+const firstName = prompt("Qual é o seu nome ?")
+const lastName = prompt("E o seu sobrenome ?")
+const studyArea = prompt("O que você estuda ?")
+const yearOfBirth = Number(prompt("Em qual ano você nasceu ?"))
+
+alert(`Olá, ${firstName} ${lastName}, vejo que atualmente está estudando ${studyArea},e, tem ${2023 - yearOfBirth} anos de idade !`)
+
+const firstNumber = Number(prompt("Digite um número"))
+const SecondNumber = Number(prompt("Digite outro número"))
+
+alert(`A soma, subtração, multiplicação e divisão desses dois números, são respectivamente : ${firstNumber + SecondNumber}; ${firstNumber - SecondNumber}; ${firstNumber * SecondNumber}; ${firstNumber / SecondNumber}.`)
+
